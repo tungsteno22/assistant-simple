39b13a6253e6cf1f851d2a0bd01bf7fc7bf81154
Nathan Avilez <tungsteno22@gmail.com>
2018-11-09 10:59:16 -0600

    it is working :)

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 var express = require('express'); // app server
@@ -28,28 +12,30 @@ app.use(bodyParser.json());
 
 // Create the service wrapper
 
-var assistant = new AssistantV1({
-  version: '2018-07-10'
-});
+var assistant = new AssistantV1(require('./secrets'));
 
 // Endpoint to be call from the client side
-app.post('/api/message', function (req, res) {
+app.post('/api/message', function(req, res) {
   var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
   if (!workspace || workspace === '<workspace-id>') {
     return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
+      output: {
+        text:
+          'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' +
+          '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' +
+          'Once a workspace has been defined the intents may be imported from ' +
+          '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.',
+      },
     });
   }
   var payload = {
     workspace_id: workspace,
     context: req.body.context || {},
-    input: req.body.input || {}
+    input: req.body.input || {},
   };
 
   // Send the input to the assistant service
-  assistant.message(payload, function (err, data) {
+  assistant.message(payload, function(err, data) {
     if (err) {
       return res.status(err.code || 500).json(err);
     }
